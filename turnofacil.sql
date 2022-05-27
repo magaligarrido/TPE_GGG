42eb5420d1bf1656bf4c16a8f8b944a979845663
GarridoMatias <mgarrido@alumnos.exa.unicen.edu.ar>
2022-05-27 15:04:10 -0300

    modificaciones en base de datos, faltan relaciones y detalles

--- turnofacil.sql
+++ turnofacil.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-05-2022 a las 11:25:54
--- Versión del servidor: 10.1.38-MariaDB
--- Versión de PHP: 7.3.2
+-- Tiempo de generación: 27-05-2022 a las 20:03:20
+-- Versión del servidor: 10.4.19-MariaDB
+-- Versión de PHP: 8.0.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,13 +24,35 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `asignacion`
+--
+
+CREATE TABLE `asignacion` (
+  `id_asigacion` int(10) NOT NULL,
+  `id_medico` int(10) NOT NULL,
+  `id_secretaria` int(10) NOT NULL,
+  `id_institucion` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `institucion`
 --
 
 CREATE TABLE `institucion` (
-  `codigo` varchar(10) NOT NULL,
-  `password` varchar(100) NOT NULL
+  `id_institucion` int(10) NOT NULL,
+  `nombre_institucion` varchar(50) NOT NULL,
+  `usuario` varchar(10) NOT NULL,
+  `pass` varchar(500) CHARACTER SET utf8mb4 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `institucion`
+--
+
+INSERT INTO `institucion` (`id_institucion`, `nombre_institucion`, `usuario`, `pass`) VALUES
+(0, 'CRUZ AZUL', 'usertest', '$2y$10$bKOaPaeslD9r4Cwu5WcGC.kbHyERnLs6SDS1A3T.9m4TAlU4WLerq');
 
 -- --------------------------------------------------------
 
@@ -40,10 +61,32 @@ CREATE TABLE `institucion` (
 --
 
 CREATE TABLE `medico` (
-  `name` varchar(20) NOT NULL,
+  `id_medico` int(10) NOT NULL,
+  `usuario` varchar(20) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `secretaria_asociada` varchar(20) NOT NULL
+  `secretaria_asociada` varchar(20) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `apellido` varchar(20) NOT NULL,
+  `id_especialidad` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pacientes`
+--
+
+CREATE TABLE `pacientes` (
+  `id_paciente` int(10) NOT NULL,
+  `dni` int(20) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `apellido` varchar(20) NOT NULL,
+  `direccion` varchar(20) NOT NULL,
+  `telefono` int(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `obra_social` varchar(20) NOT NULL,
+  `numero_afiliado` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -52,7 +95,8 @@ CREATE TABLE `medico` (
 --
 
 CREATE TABLE `secretaria` (
-  `name` varchar(20) NOT NULL,
+  `id_secretaria` int(10) NOT NULL,
+  `usuario` varchar(20) NOT NULL,
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -61,22 +105,50 @@ CREATE TABLE `secretaria` (
 --
 
 --
+-- Indices de la tabla `asignacion`
+--
+ALTER TABLE `asignacion`
+  ADD PRIMARY KEY (`id_asigacion`);
+
+--
 -- Indices de la tabla `institucion`
 --
 ALTER TABLE `institucion`
-  ADD PRIMARY KEY (`codigo`);
+  ADD PRIMARY KEY (`id_institucion`);
 
 --
 -- Indices de la tabla `medico`
 --
 ALTER TABLE `medico`
-  ADD PRIMARY KEY (`name`);
+  ADD PRIMARY KEY (`id_medico`);
+
+--
+-- Indices de la tabla `pacientes`
+--
+ALTER TABLE `pacientes`
+  ADD PRIMARY KEY (`id_paciente`);
 
 --
 -- Indices de la tabla `secretaria`
 --
 ALTER TABLE `secretaria`
-  ADD PRIMARY KEY (`name`);
+  ADD PRIMARY KEY (`id_secretaria`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `asignacion`
+--
+ALTER TABLE `asignacion`
+  MODIFY `id_asigacion` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pacientes`
+--
+ALTER TABLE `pacientes`
+  MODIFY `id_paciente` int(10) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
