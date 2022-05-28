387d35cbe17e81d4b7249a028e4aaea6280195db
GarridoMatias <mgarrido@alumnos.exa.unicen.edu.ar>
2022-05-28 17:30:18 -0300

    agrego .sql

--- turnofacil.sql
+++ turnofacil.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-05-2022 a las 19:09:14
--- Versión del servidor: 10.1.38-MariaDB
--- Versión de PHP: 7.3.2
+-- Tiempo de generación: 28-05-2022 a las 22:29:35
+-- Versión del servidor: 10.4.19-MariaDB
+-- Versión de PHP: 8.0.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -21,6 +20,25 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `turnofacil`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `especialidad`
+--
+
+CREATE TABLE `especialidad` (
+  `id_especialidad` int(10) NOT NULL,
+  `especialidad` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `especialidad`
+--
+
+INSERT INTO `especialidad` (`id_especialidad`, `especialidad`) VALUES
+(1, 'Traumatologia'),
+(2, 'Traumatologia');
 
 -- --------------------------------------------------------
 
@@ -51,20 +69,23 @@ INSERT INTO `institucion` (`id_institucion`, `nombre_institucion`, `usuario`, `p
 
 CREATE TABLE `medico` (
   `id_medico` int(10) NOT NULL,
+  `id_institucion` int(10) NOT NULL,
   `usuario` varchar(20) NOT NULL,
   `password` varchar(100) NOT NULL,
   `nombre` varchar(20) NOT NULL,
   `apellido` varchar(20) NOT NULL,
   `id_especialidad` int(20) NOT NULL,
-  `secretaria` int(10) DEFAULT NULL
+  `id_secretaria` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `medico`
 --
 
-INSERT INTO `medico` (`id_medico`, `usuario`, `password`, `nombre`, `apellido`, `id_especialidad`, `secretaria`) VALUES
-(3, 'juan12', '$2y$10$t94tLK2jvVHhBn7hG6x0ve/VcLORFcL.h8Lbfur5G.k4nEwDS1Xk.', 'juan', 'perez', 4, 9);
+INSERT INTO `medico` (`id_medico`, `id_institucion`, `usuario`, `password`, `nombre`, `apellido`, `id_especialidad`, `id_secretaria`) VALUES
+(7, 0, 'user1', '$2y$10$R6uVDcEA8HP4ycMduRrSlOpuFIPPFvIBSAX0NmZYL8HMnCDoD8K06', 'testtraumato', 'test', 1, NULL),
+(8, 0, '1', '$2y$10$0XQCIEcyTvkqvKulOvK9LOjOZ8KhPcOhFICHYfgLPjMI.RAAKCej6', '1', '12', 1, NULL),
+(9, 0, '1', '$2y$10$prHZqi/GN3hcZYAFjiZ79uCmJzlNvlbZ9E/ySk9AOK.kg3Rkrunl2', '1', '1', 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -74,6 +95,7 @@ INSERT INTO `medico` (`id_medico`, `usuario`, `password`, `nombre`, `apellido`, 
 
 CREATE TABLE `paciente` (
   `id_paciente` int(10) NOT NULL,
+  `id_institucion` int(10) NOT NULL,
   `dni` int(20) NOT NULL,
   `nombre` varchar(20) NOT NULL,
   `apellido` varchar(20) NOT NULL,
@@ -92,6 +114,7 @@ CREATE TABLE `paciente` (
 
 CREATE TABLE `secretaria` (
   `id_secretaria` int(10) NOT NULL,
+  `id_institucion` int(10) NOT NULL,
   `usuario` varchar(20) NOT NULL,
   `password` varchar(100) NOT NULL,
   `nombre` varchar(45) NOT NULL
@@ -101,14 +124,23 @@ CREATE TABLE `secretaria` (
 -- Volcado de datos para la tabla `secretaria`
 --
 
-INSERT INTO `secretaria` (`id_secretaria`, `usuario`, `password`, `nombre`) VALUES
-(5, 'pepa', '$2y$10$WvGu9lNra.2i/yCKgspJr.llV6sfFzTxlsn2mzH63ap3AHtpAbVNm', ''),
-(8, 'user', '$2y$10$Tih6IRAjUSxW9cO.h5k5qOUQTxsDoupw6NdzdBOqseZh.wU1QpPwK', 'pepa'),
-(9, 'cucu', '$2y$10$hFSWMH008aOXqs6YMcDV6uKAW2ihss61ZNcgERCFz25KOvELsm1fO', 'sara');
+INSERT INTO `secretaria` (`id_secretaria`, `id_institucion`, `usuario`, `password`, `nombre`) VALUES
+(5, 0, 'pepa', '$2y$10$WvGu9lNra.2i/yCKgspJr.llV6sfFzTxlsn2mzH63ap3AHtpAbVNm', ''),
+(8, 0, 'user', '$2y$10$Tih6IRAjUSxW9cO.h5k5qOUQTxsDoupw6NdzdBOqseZh.wU1QpPwK', 'pepa'),
+(9, 0, 'cucu', '$2y$10$hFSWMH008aOXqs6YMcDV6uKAW2ihss61ZNcgERCFz25KOvELsm1fO', 'sara'),
+(10, 0, 'S1', '$2y$10$5qCkNT5ebcfXUMD8hHUBlOu/oahSDnvSwcAfANoI1G9wkgyig2Pme', 'Secre1'),
+(11, 0, 'S2', '$2y$10$X5jn1BX4J2uXRdKifhRnIeyWMjy8p3N5EWganwSKaDmN6h4vUaBXa', 'Secre2'),
+(12, 0, 'S2', '$2y$10$qBttbRrJ7UFy23N3p28oy.jMIurtCHbTBxPt6KlyYtSJgsL8tgHaS', 'Secre2');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `especialidad`
+--
+ALTER TABLE `especialidad`
+  ADD PRIMARY KEY (`id_especialidad`);
 
 --
 -- Indices de la tabla `institucion`
@@ -121,29 +153,39 @@ ALTER TABLE `institucion`
 --
 ALTER TABLE `medico`
   ADD PRIMARY KEY (`id_medico`),
-  ADD KEY `secretaria` (`secretaria`);
+  ADD KEY `secretaria` (`id_secretaria`),
+  ADD KEY `id_institucion` (`id_institucion`),
+  ADD KEY `id_especialidad` (`id_especialidad`);
 
 --
 -- Indices de la tabla `paciente`
 --
 ALTER TABLE `paciente`
-  ADD PRIMARY KEY (`id_paciente`);
+  ADD PRIMARY KEY (`id_paciente`),
+  ADD KEY `id_institucion` (`id_institucion`);
 
 --
 -- Indices de la tabla `secretaria`
 --
 ALTER TABLE `secretaria`
-  ADD PRIMARY KEY (`id_secretaria`);
+  ADD PRIMARY KEY (`id_secretaria`),
+  ADD KEY `id_institucion` (`id_institucion`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `especialidad`
+--
+ALTER TABLE `especialidad`
+  MODIFY `id_especialidad` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT de la tabla `medico`
 --
 ALTER TABLE `medico`
-  MODIFY `id_medico` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_medico` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `paciente`
@@ -155,7 +197,7 @@ ALTER TABLE `paciente`
 -- AUTO_INCREMENT de la tabla `secretaria`
 --
 ALTER TABLE `secretaria`
-  MODIFY `id_secretaria` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_secretaria` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Restricciones para tablas volcadas
@@ -165,7 +207,21 @@ ALTER TABLE `secretaria`
 -- Filtros para la tabla `medico`
 --
 ALTER TABLE `medico`
-  ADD CONSTRAINT `medico_ibfk_1` FOREIGN KEY (`secretaria`) REFERENCES `secretaria` (`id_secretaria`);
+  ADD CONSTRAINT `medico_ibfk_1` FOREIGN KEY (`id_secretaria`) REFERENCES `secretaria` (`id_secretaria`),
+  ADD CONSTRAINT `medico_ibfk_2` FOREIGN KEY (`id_institucion`) REFERENCES `institucion` (`id_institucion`),
+  ADD CONSTRAINT `medico_ibfk_3` FOREIGN KEY (`id_especialidad`) REFERENCES `especialidad` (`id_especialidad`);
+
+--
+-- Filtros para la tabla `paciente`
+--
+ALTER TABLE `paciente`
+  ADD CONSTRAINT `paciente_ibfk_1` FOREIGN KEY (`id_institucion`) REFERENCES `institucion` (`id_institucion`);
+
+--
+-- Filtros para la tabla `secretaria`
+--
+ALTER TABLE `secretaria`
+  ADD CONSTRAINT `secretaria_ibfk_1` FOREIGN KEY (`id_institucion`) REFERENCES `institucion` (`id_institucion`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
